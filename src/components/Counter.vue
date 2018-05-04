a37da4c703153dbb9d640322303f5a898301b8fb
Jun Li <june11084@gmail.com>
2018-05-04 16:33:49 -0700

    refractor with vuex

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -24,15 +24,10 @@
 
 <script>
   import { mapActions } from 'vuex';
+  import { mapGetters } from 'vuex';
   import axios from 'axios';
   import VueAxios from 'vue-axios';
   export default {
-    data() {
-      return {
-        listOfItems: new Array("1","2","3"),
-        k:"k555555555555555k"
-      }
-    },
     computed: {
       message () {
         return this.$store.state.counter.count.toString();
@@ -40,47 +35,16 @@
       surprise () {
         return (this.$store.state.counter.count >= 5);
       },
+      ...mapGetters({
+        listOfItems: 'allResults'
+      })
     },
     methods: {
-      startLogin (){
-        function apiCall(DomainName){
-          console.log("apicall ran")
-
-            axios.get(`https://api.domainsdb.info/search?query=${DomainName}`).then((response) => {
-              let notEmpty = 0;
-              // k = `<li>Doctor: ${response.data.domains[i]} working`; console.log(k)
-              for(let i = 0; i < response.data.domains.length ; i ++){
-                console.log(response.data.domains[i]);
-                console.log(listOfItems)
-                listOfItems.push(`<li>Doctor: ${response.data.domains[i]} working`);
-                console.log(listOfItems)
-                notEmpty++;
-              }
-              if(notEmpty === 0){
-                // $('#result').append("Nothing found");
-              }
-              console.log(response.data + "response data")
-          });
-        };
-        login({
-          title: "Login",
-          message: "Please enter your user ID and Password",
-          okButtonText: "Log In",
-          cancelButtonText: "Cancel",
-          userName: "Username",
-          password: "Password"
-        }).then(result => {
-          console.log("ran");
-          console.log(`${result.userName}`);
-          console.log(this.k)
-          // callDomainSearch(`${result.userName}`);
-          apiCall(`${result.userName}`);
-        });
-      },
       ...mapActions([
         'decrement',
         'increment',
-      ]),
+        'startLogin'
+      ])
   },
 }
 </script>
--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,6 +1,14 @@
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 const state = {
-  count: 0,
+  count: 10,
+  allResults: ["1", "2", "3", "4"],
 };
+
+const getters = {
+  allResults: state => state.allResults
+}
 
 const mutations = {
   decrement (state) {
@@ -9,15 +17,50 @@ const mutations = {
   increment (state) {
     state.count++;
   },
+  startLogin (state){
+    let vm = this;
+    let responseBack = "";
+    login({
+      title: "Login",
+      message: "Please enter your user ID and Password",
+      okButtonText: "Log In",
+      cancelButtonText: "Cancel",
+      userName: "Username",
+      password: "Password"
+    }).then(result => {
+      console.log("Login Ran");
+      console.log("apicall ran");
+      responseBack = `${result.userName}`
+    });
+    return axios.get(`https://api.domainsdb.info/search?query=${responseBack}`).then((response) => {
+      console.log("api responded")
+      console.log("Api call done")
+      //console.log(response)
+      for(let i = 0; i < response.data.domains.length ; i ++){
+        console.log(response.data.domains[i]);
+        console.log(state.count)
+        vm.state.count++;
+        store.commit('decrement');
+        // let resultArray = [state.allResults];
+        //state.allResults.push(`${response.data.domains[i]}`);
+        // console.log(resultArray)
+      }
+    });
+    state.count++;
+  }
 };
 
 const actions = {
   increment: ({commit}) => commit('increment'),
-  decrement: ({commit}) => commit('decrement'),
+  decrement ({ commit }) {
+    commit('decrement')
+  },
+  startLogin: ({commit}) => commit('startLogin')
 };
 
 export default {
   state,
   mutations,
   actions,
+  getters
 };
